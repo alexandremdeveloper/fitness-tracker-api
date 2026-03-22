02bdc9b84d706d652647b5b98cb31446fc488ef5
Alexandre <avanade.amartins@voegol.com.br>
2026-03-21 22:24:53 -0300

    feat: adicionando autenticacao jwt e seguranca na api

---
+++ src/main/java/br/com/alexandre/fitnessbackend/security/JwtFilter.java
@@ -0,0 +1,47 @@
+package br.com.alexandre.fitnessbackend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                String username = jwtUtil.extractUsername(token);
+                System.out.println("Usuário autenticado: " + username);
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token inválido");
+                return;
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Token não enviado");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
